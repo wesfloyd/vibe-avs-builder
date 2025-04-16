668bc2f23889015f47b66367927213244120e69a
Wes Floyd <wesfloyd@gmail.com>
2025-04-16 12:53:42 -0400

    initial commit, still not working

--- components/chat.tsx
+++ components/chat.tsx
@@ -43,6 +43,7 @@ export function Chat({
     reload,
   } = useChat({
     id,
+    api: '/(chat)/api/chat',
     body: { id, selectedChatModel: selectedChatModel },
     initialMessages,
     experimental_throttle: 100,
